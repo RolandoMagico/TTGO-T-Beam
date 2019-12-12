b054cc44ceb858c74dc5ac3895e780eae0783836
ContextQuickie <ContextQuickie@users.noreply.github.com>
2019-12-12 15:17:09 +0100

    Fixed license text in file headers

--- Source/Components/Neo6/Neo6.c
+++ Source/Components/Neo6/Neo6.c
@@ -1,22 +1,18 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the LICENSE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
+/***************************************************************************************************
+ * Copyright 2019 ContextQuickie
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **************************************************************************************************/
 
 /***************************************************************************************************
  * INCLUDES
@@ -87,7 +83,7 @@ Neo6_BoolType Neo6_DataAvailable(size_t* dataLength)
   return (*dataLength) > 0;
 }
 
-int Neo6_GetReceivedData(uint8_t* buffer, uint32_t bufferLength)
+int Neo6_GetReceivedData(uint8_t* buffer)
 {
   uint32_t reultBufferLength = Neo6_ReadBufferLength;
   Neo6_MemCopy(Neo6_ReadBuffer, buffer, reultBufferLength);
