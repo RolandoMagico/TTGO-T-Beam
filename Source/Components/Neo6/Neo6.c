bc36a2e43984c92828f89b172ebff1d6c7b4b0a8
ContextQuickie <ContextQuickie@users.noreply.github.com>
2019-12-12 12:27:32 +0100

    GPS UART working

--- Source/Components/Neo6/Neo6.c
+++ Source/Components/Neo6/Neo6.c
@@ -41,17 +41,28 @@ void Neo6_InitMemory()
 
 void Neo6_Init()
 {
+  QueueHandle_t uart_queue;
   uart_config_t uart_config = {
       .baud_rate = 9600,
       .data_bits = UART_DATA_8_BITS,
       .parity = UART_PARITY_DISABLE,
       .stop_bits = UART_STOP_BITS_1,
-      .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
-      .rx_flow_ctrl_thresh = 122,
+      .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
   };
   // Configure UART parameters
   ESP_ERROR_CHECK(uart_param_config(NEO6_UART_PERIPHERAL, &uart_config));
 
   // Set UART pins
   ESP_ERROR_CHECK(uart_set_pin(NEO6_UART_PERIPHERAL, NEO6_UART_TX_PIN, NEO6_UART_RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+
+  // Install UART driver using an event queue here
+  ESP_ERROR_CHECK(uart_driver_install(NEO6_UART_PERIPHERAL, NEO6_UART_BUFFER_SIZE, NEO6_UART_BUFFER_SIZE, 10, &uart_queue, 0));
+
+  ESP_ERROR_CHECK(uart_flush(NEO6_UART_PERIPHERAL));
+}
+
+Neo6_BoolType Neo6_DataAvailable(size_t* dataLength)
+{
+  ESP_ERROR_CHECK(uart_get_buffered_data_len(NEO6_UART_PERIPHERAL, dataLength));
+  return (*dataLength) > 0;
 }
